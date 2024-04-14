74683fea57dd32013a8f2bb14c39aeb658412474
madhu <166997159+gothicLettuce@users.noreply.github.com>
2024-04-14 17:02:15 +0530

    Delete chat server directory

--- chat server/compile.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("clang++ -Iincludes/ source/main.cpp source/serverC.cpp -lws2_32 -o main")
-os.system(".\main")
--- chat server/compile_client.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("clang++ -Iincludes/ source/client.cpp -lws2_32 -o client")
-os.system(".\client")
